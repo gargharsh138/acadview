952dba23a9ca801c9b8da50e1b17276e22405efb
gargharsh138 <garg.harsh138@gmail.com>
2017-09-06 21:59:21 +0530

    default user or new user

--- spychat.py
+++ spychat.py
@@ -1,27 +1,37 @@
+#import statements
+from spydetails import name,salutation,age,rating
+
 print "Welcome to Spy Chat. It's good to have you"
-name=raw_input("Please enter your name: ")                          #variable for spy name
-salutation=raw_input("What should we call you Mr. or Mrs.?: ")      #variable for salutation
-if len(name) > 0:
-    age = 0               #spy age
-    rating = 0.0          #spy rating
-    status = False        #spy status
-    print "Welcome "+salutation+" "+name
-    print "We would like to know more about you"
-    age=int(raw_input("Please enter your age: "))           #input spy age
-    if (age < 12 or age > 50):                              #condition to check valid age of spy
-        print "Sorry please enter valid age."
+question = raw_input("Would you like to continue as "+salutation+""+name+" (Y/N): ")    #default user or new user
+if question == "y" or question == "Y":
+    print "Name: %s |Age: %s |Rating: %s " % (name, str(age), str(rating))  # final details of spy
+
+elif question == "N" or question == "n":
+    name=raw_input("Please enter your name: ")                          #variable for spy name
+    salutation=raw_input("What should we call you Mr. or Mrs.?: ")      #variable for salutation
+    if len(name) > 0:
+        age = 0               #spy age
+        rating = 0.0          #spy rating
+        status = False        #spy status
+        print "Welcome "+salutation+" "+name
+        print "We would like to know more about you"
+        age=int(raw_input("Please enter your age: "))           #input spy age
+        if (age < 12 or age > 50):                              #condition to check valid age of spy
+            print "Sorry please enter valid age."
+        else:
+            rating=float(raw_input("Please enter your rating: "))   #input spy rating
+            if rating > 4.5:                                        #print messages according to spy rating
+                print "Great ace!!"
+            elif rating > 3.5 and rating <= 4.5 :
+                print "Nice work!!!"
+            elif rating > 2.5 and rating <= 3.5 :
+                print "Keep it up!!!"
+            else :
+                print "Work hard!!!"
+            status = True
+            print "Thanks for all the info,here are your details: "
+            print "Name: %s |Age: %s |Rating: %s "%(name,str(age),str(rating))  #final details of spy
     else:
-        rating=float(raw_input("Please enter your rating: "))   #input spy rating
-        if rating > 4.5:                                        #print messages according to spy rating
-            print "Great ace!!"
-        elif rating > 3.5 and rating <= 4.5 :
-            print "Nice work!!!"
-        elif rating > 2.5 and rating <= 3.5 :
-            print "Keep it up!!!"
-        else :
-            print "Work hard!!!"
-        status = True
-        print "Thanks for all the info,here are your details: "
-        print "Name: %s |Age: %s |Rating: %s "%(name,str(age),str(rating))
+        print "Sorry Invalid user name.Please try again."
 else:
-    print "Sorry Invalid user name.Please try again."
+    print "Not valid Input.Please try again."
---
+++ spydetails.py
@@ -0,0 +1,5 @@
+name = "Bond"
+salutation = "Mr. "
+age =  27
+rating = 4.9
+status = True
